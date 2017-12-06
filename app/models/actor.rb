890ca50cb8234c17cc2a82e6343ebde5ce1aab5e
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-12-06 12:19:18 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,8 @@
 class Actor < ActiveRecord::Base
     has_many :characters
     has_many :shows, through: :characters
+
+    def full_name
+      self.pluck(:first_name, :last_name).join(" ")
+    end
 end
