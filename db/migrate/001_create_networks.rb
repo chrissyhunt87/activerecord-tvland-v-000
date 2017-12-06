64f721921ebe520f07acb77cd1bd69be5c542710
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-12-06 12:07:24 +0000

    automatically backed up by learn

--- db/migrate/001_create_networks.rb
+++ db/migrate/001_create_networks.rb
@@ -1,6 +1,6 @@
 class CreateNetworks < ActiveRecord::Migration
   def change
-    create_table :networks do |t| 
+    create_table :networks do |t|
       t.string :call_letters
       t.integer :channel
     end
