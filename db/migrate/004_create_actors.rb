12ffa713c473fb2ea1d424a6f952b178077a68ee
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-12-06 12:14:31 +0000

    automatically backed up by learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,7 +1,10 @@
 class CreateActors < ActiveRecord::Migration[4.2]
   def change
     create_table :actors do |t|
-      t.string :name
+      t.string :first_name
+      t.string :last_name
+      t.integer :character_id
+      t
     end
   end
 end
